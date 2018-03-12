b4555a7b110a369ad5429c359466de0557f49676
Ryan McDonagh <laslomcd@gmail.com>
2018-03-12 14:06:13 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LeftComponentComponent } from './left-component/left-component.component';
+import { RightComponentComponent } from './right-component/right-component.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeftComponentComponent,
+    RightComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
